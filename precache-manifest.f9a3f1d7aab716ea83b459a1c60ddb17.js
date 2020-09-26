b5c72ca3cbf22db1bd249843d716a374655f6565
Florin <flo@dinnerbooking.com>
2020-09-26 09:51:15 +0300

    Updates

---
+++ precache-manifest.f9a3f1d7aab716ea83b459a1c60ddb17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10af23722c4275a832755836cf92fd63",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e2f1c23d1318727afa42",
+    "url": "/robots/static/css/main.3e035f1a.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robots/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f1c23d1318727afa42",
+    "url": "/robots/static/js/main.4f5f3f3f.chunk.js"
+  },
+  {
+    "revision": "df9e66469025a98d5aad",
+    "url": "/robots/static/js/runtime-main.e1a61dd3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
